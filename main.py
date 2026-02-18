58377933c845c3f96b94837549b66ffb7da9e0b7
Nick Steinmann <nick.steinmann@gmx.de>
2026-02-18 18:21:48 +0100

    Adding the feature to coose to replay again or exit the programm, instaed of closing after finishing or making 3 mistakes

--- main.py
+++ main.py
@@ -6,4 +6,17 @@ Entry point for the Snowman game.
 from game_logic import play_game
 
 if __name__ == "__main__":
-    play_game()
+    while True:
+        play_game()
+
+        while True:
+            restart = input("Would you like to play again? (y/n): ").lower()
+
+            if restart == "y":
+                print("Restarting the game...")
+                break  # verlässt nur die innere Schleife
+            elif restart == "n":
+                print("Goodbye!")
+                exit()
+            else:
+                print("Please enter y or n.")
