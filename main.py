208c04cd70b6f47eb4cba8108ef255cd9decdc59
Nick Steinmann <nick.steinmann@gmx.de>
2026-02-18 16:26:21 +0100

    Melting the Snowman with wrong anwsers

--- main.py
+++ main.py
@@ -39,25 +39,60 @@ def get_random_word():
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
+def display_game_state(mistakes, secret_word, guessed_letters):
+    # Display the snowman stage for the current number of mistakes.
+    print(STAGES[mistakes])
+    # Build a display version of the secret word.
+    display_word = ""
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word)
+    print("\n")
+
+
 def play_game():
     secret_word = get_random_word()
+    guessed_letters = []
+    mistakes = 0
+
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: ", secret_word)
+    display_game_state(mistakes, secret_word, guessed_letters)
     print(spaceholders(secret_word))
 
     # TODO: Build your game loop here.
     # For now, simply prompt the user once:
-    guess = asking_player()
+
+    guess = asking_player(secret_word, mistakes)
+    guessed_letters.append(guess)
 
 
 def spaceholders(secret_word):
     return "_" * len(secret_word)
 
 
-def asking_player():
-    guess = input("Guess a letter: ").lower()
-    print("You guessed: " + guess)
-    return guess
+def check_letter(secret_word, guess):
+    if guess in secret_word:
+        return True
+
+
+def asking_player(secret_word, mistakes):
+    mistakes = 0
+    while mistakes < 3:
+        guess = input("Guess a letter: ").lower()
+        check_if_correct = check_letter(secret_word, guess)  # -> bool
+        if check_if_correct:
+            print("You guessed: " + guess)
+            print(STAGES[mistakes])
+            return guess
+        else:
+            print("Wrong guess. Try again.")
+            mistakes += 1
+            print(STAGES[mistakes])
+    print("You lost. Try again.")
 
 
 if __name__ == "__main__":
